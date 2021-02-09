818f61755b3cd64bf9bedbd8aac137a14d635082
hyungyu Kim <ypsophila97@icloud.com>
2021-02-09 16:11:57 +0900

    Implement POST request

--- alamofireTutorial2-iOS/ViewController.swift
+++ alamofireTutorial2-iOS/ViewController.swift
@@ -23,35 +23,48 @@ class ViewController: UIViewController {
     @IBAction func onClickedGetBtn(_ sender: Any) {
         getTest()
     }
+    @IBAction func onClickedPostBtn(_ sender: Any) {
+        postTest()
+    }
     //MARK: - GET,POST methods
     func getTest() {
         let url = "https://jsonplaceholder.typicode.com/todos/1"
         AF.request(url).validate(statusCode: 200..<300)
             .responseJSON{
-                (response) in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-                    let userId = json["userId"].intValue
-                    let id = json["id"].intValue
-                    let title = json["title"].stringValue
-                    print("ViewController - GET")
+            (response) in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let userId = json["userId"].intValue
+                let id = json["id"].intValue
+                let title = json["title"].stringValue
+                print("ViewController - GET")
                     
-                    self.userIdLabel.text = String(userId)
-                    self.idLabel.text = String(id)
-                    self.titleLabel.text = title
+                self.userIdLabel.text = String(userId)
+                self.idLabel.text = String(id)
+                self.titleLabel.text = title
                     
-                case .failure(let err):
+            case .failure(let err):
                     //나라별 언어로 표현.
                     print(err.localizedDescription)
-                }
             }
+        }
     }
     func postTest() {
-        let url = "/t/nzzq2-1612844842/post"
-        
-        
+        let login: [String: String] = [
+            "email": "1234@1234.com",
+            "password": "1234"
+        ]
+        let header: HTTPHeaders = [ "Content-Type" : "application/json" ]
+        let url = "https://ptsv2.com/t/eoxz4-1612853643/post"
+        AF.request(url, method: .post, parameters: login,encoding: JSONEncoding.default,headers: header).responseJSON{ (response) in
+            switch response.result{
+            case .success:
+                print("POST success!")
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
     }
-
 }
 
